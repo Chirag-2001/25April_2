27f752c324119fe5c3361a303387a7b413c821a7
Prajwalluche <prajwal.luche@gmail.com>
2023-04-25 16:50:33 +0530

    third commit

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+# Install dependencies
+npm install
+
+# Build the code using webpack
+npx webpack --mode production
+
+# Run unit tests
+npm run test
+
+# Run integration tests
+npm run integration-test
+
